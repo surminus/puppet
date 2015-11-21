2053aeff71ac525e3ed4ca110b86c8c11b5cecc4
Paul Martin <surminus@gmail.com>
2015-11-21 08:23:52 +0000

    Created for testing
    Useful guide:
    https://www.digitalocean.com/community/tutorials/how-to-set-up-a-masterless-puppet-environment-on-ubuntu-14-04

---
+++ modules/puppet-sync/manifests/init.pp
@@ -0,0 +1,17 @@
+class puppet-sync {
+    file { 'post-hook':
+        ensure  => file,
+        path    => '/etc/puppet/.git/hooks/post-merge',
+        source  => 'puppet:///modules/puppet-sync/post-merge',
+        mode    => 0755,
+        owner   => root,
+        group   => root,
+    }
+    cron { 'puppet-apply':
+        ensure  => present,
+        command => "cd /etc/puppet ; /usr/bin/git pull",
+        user    => root,
+        minute  => '*/30',
+        require => File['post-hook'],
+    }
+}
