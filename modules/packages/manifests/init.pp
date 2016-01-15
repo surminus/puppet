953172da30bce662592880301e485e7d0d16c3aa
Paul Martin <Paul.martin@digital.cabinet-office.gov.uk>
2016-01-15 10:13:57 +0000

    Refine package list
     - Tidy package list
     - Add postfix as MTA

--- modules/packages/manifests/init.pp
+++ modules/packages/manifests/init.pp
@@ -1,7 +1,15 @@
 class packages {
-  $software = [ 'screen', 'git' ]
+  $software = [
+    'screen',
+    'git',
+    'postfix',
+  ]
 
   package {
     $software: ensure => 'installed'
+  }
+
+  service { 'postfix':
+    ensure => running
   }
 }
