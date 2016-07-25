05560fcb24f50dd0653c099de5470d405f93301d
Laura Martin <surminus@gmail.com>
2016-07-25 13:22:09 +0100

    Revert cron
    I don't think this is needed.

--- modules/blog/manifests/init.pp
+++ modules/blog/manifests/init.pp
@@ -20,11 +20,4 @@ class blog (
 
   rbenv::compile { $ruby_version: user => $user, home => $home, global => true }
   rbenv::install { $user: group => $user, home => $home }
-
-  cron { 'jekyll blog':
-    command => 'cd ~/blog; pgrep -f jekyll >/dev/null 2>&1 || bundle exec jekyll serve --detach',
-    user    => 'laura',
-    minute  => '*/5',
-    require => Vcsrepo["${home}/blog"]
-  }
 }
