3cb7a3b36722dbd4b035509039906004e6bd33b1
Laura Martin <surminus@gmail.com>
2017-03-22 11:37:03 +0000

    Update puppet
    Update puppet so it runs every 15 minutes regardless of any changes to
    the puppet repo. This is because there is a resource to pull the latest
    repo of a website I'm hosting, and I want changes to be checked every 15
    minutes.

--- modules/puppet_sync/manifests/init.pp
+++ modules/puppet_sync/manifests/init.pp
@@ -1,18 +1,21 @@
 # == Class: Puppet_sync
 class puppet_sync {
     file { 'post-merge':
-        ensure => file,
+        ensure => absent,
         path   => '/etc/puppet/.git/hooks/post-merge',
-        source => 'puppet:///modules/puppet_sync/post-merge',
+    }
+    file { '/usr/local/bin/run-puppet':
+        ensure => file,
+        source => 'puppet:///modules/puppet_sync/run-puppet',
         mode   => '0755',
         owner  => root,
         group  => root,
     }
     cron { 'puppet-apply':
         ensure  => present,
-        command => 'cd /etc/puppet ; /usr/bin/git pull',
+        command => '/usr/local/bin/run-puppet >/dev/null 2>&1',
         user    => root,
         minute  => '*/15',
-        require => File['post-merge'],
+        require => File['/usr/local/bin/run-puppet'],
     }
 }
